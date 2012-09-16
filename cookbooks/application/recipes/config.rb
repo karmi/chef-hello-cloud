36b61f63c6c8d49f4eea0b7a6c33422d8d7c044f
Karel Minarik <karmi@karmi.cz>
2012-09-16 19:11:01 +0200

    WIP > Fixed incorrect PostgreSQL config in `pg_hba.conf`

--- cookbooks/application/recipes/config.rb
+++ cookbooks/application/recipes/config.rb
@@ -2,11 +2,16 @@
 #
 if node.attribute?(:vagrant) and node.attribute?(:postgresql)
 
-  bash "allow PostgreSQL clients connecting to 33.*.*.* IPs" do
+  bash "allow PostgreSQL clients connecting to 33.33.33.* IPs" do
     code <<-COMMAND
-    echo 'host    all         all         33.33.33.10/19        md5' >> /etc/postgresql/9.1/main/pg_hba.conf
+    echo ''                                                         >> /etc/postgresql/9.1/main/pg_hba.conf
+    echo '# Allow connections from application'                     >> /etc/postgresql/9.1/main/pg_hba.conf
+    echo 'host    all         all         33.33.33.0/24        md5' >> /etc/postgresql/9.1/main/pg_hba.conf
     COMMAND
-    not_if "grep 33.33.33 /etc/postgresql/9.1/main/pg_hba.conf"
+
+    not_if "grep 33.33.33.0 /etc/postgresql/9.1/main/pg_hba.conf"
+
+    notifies :restart, "service[postgresql]"
   end
 
 end
