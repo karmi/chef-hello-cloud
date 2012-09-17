ca6d87af60a08b9cd125de1f31f8071e0adc75f6
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-18 00:09:56 +0200

    WIP > Fixed opening up PG to external clients

--- cookbooks/application/recipes/config.rb
+++ cookbooks/application/recipes/config.rb
@@ -1,15 +1,15 @@
 # When running in Vagrant, we need clients to connect to PostgreSQL via the eth1 IPs
 #
-if node.attribute?(:vagrant) and node.attribute?(:postgresql)
+if node.attribute?(:cloud) and node.attribute?(:postgresql)
 
-  bash "allow PostgreSQL clients connecting to 33.33.33.* IPs" do
+  bash "allow external PostgreSQL clients" do
     code <<-COMMAND
-    echo ''                                                         >> /etc/postgresql/9.1/main/pg_hba.conf
-    echo '# Allow connections from application'                     >> /etc/postgresql/9.1/main/pg_hba.conf
-    echo 'host    all         all         33.33.33.0/24        md5' >> /etc/postgresql/9.1/main/pg_hba.conf
+    echo ''                                                         >> #{node.postgresql[:dir]}/pg_hba.conf
+    echo '# Allow connections from outside'                         >> #{node.postgresql[:dir]}/pg_hba.conf
+    echo 'host    all         all         0.0.0.0/0            md5' >> #{node.postgresql[:dir]}/pg_hba.conf
     COMMAND
 
-    not_if "grep 33.33.33.0 /etc/postgresql/9.1/main/pg_hba.conf"
+    not_if "grep 0.0.0.0/0 #{node.postgresql[:dir]}/pg_hba.conf"
 
     notifies :restart, "service[postgresql]"
   end
