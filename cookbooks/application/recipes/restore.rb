7a0efac85feef845eead870aa86c1a751c5a5094
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-17 17:01:16 +0200

    WIP > Fixed the "application::restore" recipe to just feed dump to `psql` and don't use Rake

--- cookbooks/application/recipes/restore.rb
+++ cookbooks/application/recipes/restore.rb
@@ -1,36 +1,11 @@
-ruby_block "create the database" do
-  block do
-    command = <<-COMMAND
-      sudo su - #{node.application[:user]} -m -c '
-        LC_CTYPE="en_US.UTF-8" cd #{node.application[:dir]}/#{node.application[:name]} && \
-        bundle exec rake db:create && \
-        bundle exec rake db:schema:load
-      '
-    COMMAND
-    Chef::Log.debug command
-
-    system command
-  end
-
-  not_if do
-    command = <<-COMMAND
-      LC_CTYPE="en_US.UTF-8" PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
-        --host=#{ENV['POSTGRESQL_HOST']} \
-        --username=postgres \
-        --dbname=gemcutter_development \
-        --command '' > /dev/null 2>&1
-    COMMAND
-    Chef::Log.debug command
-
-    system command
-  end
-end
-
 ruby_block "restore the database" do
+  # NOTE: Dump created with
+  #       $ pg_dump --format=p --no-owner --no-privileges --exclude-table=delayed_jobs gemcutter_development
+  #
   block do
     command = <<-COMMAND
       curl -# https://s3.amazonaws.com/webexpo-chef/gemcutter_development.sql | \
-      LC_CTYPE="en_US.UTF-8" PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
+      PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
         --host=#{ENV['POSTGRESQL_HOST']} \
         --username=postgres \
         --dbname=gemcutter_development
@@ -40,14 +15,13 @@ ruby_block "restore the database" do
     system command
   end
 
-  only_if do
+  not_if do
     command = <<-COMMAND
-      LC_CTYPE="en_US.UTF-8" PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
+      PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
         --host=#{ENV['POSTGRESQL_HOST']} \
         --username=postgres \
         --dbname=gemcutter_development \
-        --command 'SELECT id FROM rubygems LIMIT 1;' | \
-      grep '0 rows'
+        --command '' > /dev/null 2>&1
     COMMAND
     Chef::Log.debug command
 
