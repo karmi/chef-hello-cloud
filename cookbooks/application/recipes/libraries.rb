f541b9a449505f1f3bb50e309a751fd1306dadf5
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-17 23:53:18 +0200

    WIP > Added openssl packages to application::libraries

--- cookbooks/application/recipes/libraries.rb
+++ cookbooks/application/recipes/libraries.rb
@@ -1,6 +1,13 @@
 libraries = value_for_platform(
-  ["ubuntu", "debian"]             => { "default" => ["libyaml-dev", "libxml2-dev",   "libxslt-dev"]    },
-  [ "centos", "redhat", "amazon" ] => { "default" => ["libyaml-devel", "libxml2-devel", "libxslt-devel"]  }
+  ["ubuntu", "debian"]             => { "default" => [ "libyaml-dev",
+                                                       "libxml2-dev",
+                                                       "libxslt-dev",
+                                                       "libssl-dev" ]    },
+
+  [ "centos", "redhat", "amazon" ] => { "default" => [ "libyaml-devel",
+                                                       "libxml2-devel",
+                                                       "libxslt-devel",
+                                                       "openssl-devel" ]  }
 )
 
 libraries.each do |name|
