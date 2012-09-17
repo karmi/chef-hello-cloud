dacb7a954a840a8f3887f9a119b65527d78fc763
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-17 17:09:57 +0200

    WIP > Added the "create database" step back into application::restore

--- cookbooks/application/recipes/restore.rb
+++ cookbooks/application/recipes/restore.rb
@@ -1,3 +1,31 @@
+ruby_block "create the database" do
+  block do
+    command = <<-COMMAND
+      PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} createdb \
+        --host=#{ENV['POSTGRESQL_HOST']} \
+        --username=postgres \
+        --owner=postgres \
+        gemcutter_development
+    COMMAND
+    Chef::Log.debug command
+
+    system command
+  end
+
+  not_if do
+    command = <<-COMMAND
+      PGPASSWORD=#{ENV['POSTGRESQL_PASSWORD']} psql \
+        --host=#{ENV['POSTGRESQL_HOST']} \
+        --username=postgres \
+        --dbname=gemcutter_development \
+        --command '' > /dev/null 2>&1
+    COMMAND
+    Chef::Log.debug command
+
+    system command
+  end
+end
+
 ruby_block "restore the database" do
   # NOTE: Dump created with
   #       $ pg_dump --format=p --no-owner --no-privileges --exclude-table=delayed_jobs gemcutter_development
