f144811ef36d2dc7bf31184f1788c119ab4c3cfe
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-18 23:07:20 +0200

    Added role: database
    Installs and configures:
    
    * PostgreSQL
    * Redis

---
+++ roles/database.rb
@@ -0,0 +1,12 @@
+name        "database"
+description "Configuration for database servers (Redis, PostgreSQL)"
+
+run_list    "role[base]",
+            "recipe[redisio::install]",
+            "recipe[redisio::enable]",
+            "recipe[postgresql::server]"
+
+override_attributes \
+  "postgresql" => {
+    "listen_addresses" => "*"
+  }
