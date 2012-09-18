d91c64da593194cbc79ccff4c5eaa0122276327b
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-18 12:37:31 +0200

    WIP > Removed "java" recipe from elasticsearch

--- roles/elasticsearch.rb
+++ roles/elasticsearch.rb
@@ -2,7 +2,6 @@ name        "elasticsearch"
 description "Configuration for elasticsearch nodes"
 
 run_list    "role[base]",
-            "recipe[java]",
             "recipe[nginx]",
             "recipe[elasticsearch]",
             "recipe[elasticsearch::plugin_aws]",
