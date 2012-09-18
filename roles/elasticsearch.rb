8754de8b2f8423f7457bb2484a7880604d4a3c48
Karel Minarik & Vojtech Hyza <dev@socialinsider.cz>
2012-09-18 23:09:36 +0200

    Added role: elasticsearch
    Installs and configures elasticsearch for EC2 with Nginx proxy

---
+++ roles/elasticsearch.rb
@@ -0,0 +1,25 @@
+name        "elasticsearch"
+description "Configuration for elasticsearch nodes"
+
+run_list    "role[base]",
+            "recipe[nginx]",
+            "recipe[elasticsearch]",
+            "recipe[elasticsearch::plugin_aws]",
+            "recipe[elasticsearch::proxy_nginx]"
+
+override_attributes(
+  "elasticsearch" => {
+    "cluster_name" => "chef-hello-cloud",
+    "discovery"    => {
+      "type" => "ec2"
+    },
+    "cloud" => {
+      "aws" => { "access_key" => ENV["AWS_ACCESS_KEY_ID"], "secret_key" => ENV["AWS_SECRET_ACCESS_KEY"] },
+      "ec2" => { "security_group" => "elasticsearch" }
+    },
+    "nginx" => {
+      "users" => [{ "username" => 'hello', "password" => 'cloud' }],
+      "allow_cluster_api" => true
+    }
+  }
+)
