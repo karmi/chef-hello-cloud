3c6f0d611ce5e21e8ab6cd742eda879140e180cf
Vojtech Hyza <vhyza@vhyza.eu>
2012-09-19 22:06:57 +0200

    [WIP][MONIT] Enabled redis and postgresql monitoring

--- roles/database.rb
+++ roles/database.rb
@@ -5,7 +5,11 @@ run_list    "role[base]",
             "recipe[redisio::install]",
             "recipe[redisio::enable]",
             "recipe[postgresql::server]",
-            "recipe[application::config]"
+            "recipe[application::config]",
+            "recipe[monit]",
+            "recipe[monitoring]",
+            "recipe[monitoring::redis]",
+            "recipe[monitoring::postgresql]"
 
 override_attributes \
   "postgresql" => {
