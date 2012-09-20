a2cd295e6f4c53dbfe45376f763be84097bb5c87
Vojtech Hyza <vhyza@vhyza.eu>
2012-09-20 21:35:31 +0200

    [WIP][MONIT] Changed monit start delay

--- cookbooks/monitoring/attributes/default.rb
+++ cookbooks/monitoring/attributes/default.rb
@@ -1,6 +1,6 @@
 default.monit[:http_auth]    = { :username => "monit", :password => "monit" }
 default.monit[:poll_period]  = 60
-default.monit[:start_delay]  = 0
+default.monit[:start_delay]  = 60
 
 default.monit[:log]          = '/var/log/monit.log'
 default.monit[:pid]          = '/var/run/monit.pid'
